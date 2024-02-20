5d6ef618397ae870e7b14f561c8426f0966993a6
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2024-02-19 21:57:07 -0600

    Update latent_to_score_vae.py

--- latent_to_score_vae.py
+++ latent_to_score_vae.py
@@ -188,8 +188,8 @@ train_size = int(0.8 * len(dataset))
 test_size = len(dataset) - train_size
 train_dataset, test_dataset = random_split(dataset, [train_size, test_size])
 
-train_dataloader = DataLoader(train_dataset, batch_size=32, shuffle=True)
-test_dataloader = DataLoader(test_dataset, batch_size=32, shuffle=False)
+train_dataloader = DataLoader(train_dataset, batch_size=320, shuffle=True)
+test_dataloader = DataLoader(test_dataset, batch_size=320, shuffle=False)
 
 model = VAEScorePredictor(LATENT_DIM).to(device)
 for param in model.encoder.parameters():
